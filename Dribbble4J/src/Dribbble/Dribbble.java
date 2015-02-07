7d0ca0492a1ef4e98885cc052243fe6b7707b015
newmlee <matthewlee0725@gmail.com>
2015-02-07 14:13:05 +0000

    Rebuilding Network/*

--- Dribbble4J/src/Dribbble/Dribbble.java
+++ Dribbble4J/src/Dribbble/Dribbble.java
@@ -1,35 +1,12 @@
 package Dribbble;
 
-import Network.Parameters;
-import Network.Service;
-import Network.Rest;
-import retrofit.RequestInterceptor;
-import retrofit.RestAdapter;
-
 public class Dribbble {
     private String accessToken;
     public String getAccessToken() {
         return accessToken;
     }
 
-    private Service.UserService userService;
-    public Service.UserService getUserService() {
-        return userService;
-    }
-
     public Dribbble(final String accessToken) {
         this.accessToken = accessToken;
-
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setRequestInterceptor(new RequestInterceptor() {
-                    @Override
-                    public void intercept(RequestFacade requestFacade) {
-                        requestFacade.addHeader(Parameters.AUTHORIZATION, " " + Parameters.BEARER + " " + accessToken);
-                    }
-                })
-                .setEndpoint(Rest.SCHEMA)
-                .build();
-
-        userService = restAdapter.create(Service.UserService.class);
     }
 }
